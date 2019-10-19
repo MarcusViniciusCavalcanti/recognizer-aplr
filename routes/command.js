6bb9cbd733c442ae076eb8eb11cabff1d197c3da
vinicius <vinicius@pcs-MacBook-Pro-2.local>
2019-10-19 11:31:52 -0300

    added control relay

--- routes/command.js
+++ routes/command.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Gpio = require('onoff').Gpio;
+
+
+const relay = new Gpio(23, 'out');
 
 /* GET users listing. */
 router.get('/open', function(req, res, next) {
+    if (relay.readSync() === 0) {
+      setInterval(() => {
+          relay.writeSync(Gpio.HIGH);
+          relay.writeSync(Gpio.LOW);
+      }, 500);
+    }
     res.send({status: 1, message: 'abrindo cancela'});
 });
 
